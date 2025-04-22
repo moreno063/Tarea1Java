52580c4219986c053668b2b1af09cc4ba95543f6
estudiante <estudiante@10.220.4.144>
2025-04-22 17:48:41 -0500

    3. no se

---
+++ 12ff/src/main/java/com/mycompany/ff/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ff;
+
+/**
+ *
+ * @author estudiante
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("443322");
+    }
+}
